1131d1a2c7d1328f448b82793a3866a125e0f6f3
Louisa007 <55337747+Louisa007@users.noreply.github.com>
2020-06-29 05:26:51 +0900

    Add files via upload

---
+++ Есаулова_База данных/PetsDB_test1/PetsDB_test1/Pet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsDB_test1
+{
+    public class Pet
+    {
+        public Pet
+            (
+               ushort rowID,
+               string ownerName,
+               string petKind,
+               string petBreed,
+               string petGender,
+               string petName,
+               ushort petAge
+            )
+        {
+            if ((ownerName == "") || (petKind == "") || (petBreed == "") || (petGender == "") || (petName == ""))
+            {
+                throw new Exception("Проверьте, всё ли вы заполнили. Все поля должны быть заполнены");
+            }
+
+            RowID = rowID;
+            OwnerName = ownerName;
+            PetKind = petKind;
+            PetBreed = petBreed;
+            PetGender = petGender;
+            PetName = petName;
+            PetAge = petAge;
+
+            if ((petAge < 1) || (petAge > 50))
+            {
+                throw new Exception("Возраст питомца должен быть не меньше года, и не больше 50");
+            }
+
+            PetAge = petAge;
+            RowID = rowID;
+        }
+
+        // Свойства для полей
+        public ushort RowID     { get; set; }  // | ID записи в таблице  
+        public string OwnerName { get; set; }  // | Имя хозяина
+        public string PetKind   { get; set; }  // | Вид питомца (кошка, собака, попугай, сова ...)
+        public string PetBreed  { get; set; }  // | Порода питомца
+        public string PetGender { get; set; }  // | Пол питомца
+        public string PetName   { get; set; }  // | Кличка питомца
+        public ushort PetAge    { get; set; }  // | Возраст питомца
+
+        public override string ToString()
+        {
+            return RowID + " | " + OwnerName + " | " + PetKind + " | " + PetBreed + " | " + PetGender + " | " + PetName + " | " + PetAge + " | ";
+        }
+    }
+}
---
+++ Есаулова_База данных/PetsDB_test1/PetsDB_test1/SearchForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetsDB_test1
+{
+    public partial class SearchForm : Form
+    {
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_SearchOK_Click(object sender, EventArgs e)
+        {
+            MainForm f = Owner as MainForm;
+
+            if ((f.data.PetsFile.Count == 0) || (textBox_Search.Text == ""))
+                return;
+
+            f.dataGrid.ClearSelection();
+            List<int> foundElements = f.data.SearchRows(textBox_Search.Text);
+
+            if (foundElements[0] == -1)
+            {
+                MessageBox.Show("Ничего не удалось найти!");
+                return;
+            }
+
+            f.dataGrid.CurrentCell = f.dataGrid[0, foundElements[0]];
+            for (int i = 0; i < foundElements.Count; i++)
+            {
+                f.dataGrid.Rows[foundElements[i]].Selected = true;
+            }
+        }
+    }
+}
